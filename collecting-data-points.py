8d440d84575b758bedf3cf19daf087da8b2b62f7
migaldottir <none399@outlook.com>
2022-09-25 19:34:39 +0200

    checking if there are enough data points within the dataset

---
+++ collecting-data-points.py
@@ -0,0 +1,7 @@
+minimum_points = 100
+data_points = 150
+
+if data_points >= minimum_points:
+    print("There are enough data ponits!")
+if data_points < minimum_points:
+    print("Keep collecting data.")
